f69b633e3cce0eff140474528c0716708d6a06ff
Benjamin Navarro <navarro@lirmm.fr>
2017-09-04 18:13:01 +0200

    updated license

--- src/OpenPHRI/utilities/polynomial_root_finder.cpp
+++ src/OpenPHRI/utilities/polynomial_root_finder.cpp
@@ -1,21 +1,22 @@
-/* 	File: polynomial_root_finder.cpp
-*	This file is part of the program open-phri
-*  	Program description : OpenPHRI: a generic framework to easily and safely control robots in interactions with humans
-*  	Copyright (C) 2017 -  Benjamin Navarro (LIRMM). All Right reserved.
+/*      File: polynomial_root_finder.cpp
+*       This file is part of the program open-phri
+*       Program description : OpenPHRI: a generic framework to easily and safely control robots in interactions with humans
+*       Copyright (C) 2017 -  Benjamin Navarro (LIRMM). All Right reserved.
 *
-*	This software is free software: you can redistribute it and/or modify
-*	it under the terms of the LGPL license as published by
-*	the Free Software Foundation, either version 3
-*	of the License, or (at your option) any later version.
-*	This software is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*	LGPL License for more details.
+*       This software is free software: you can redistribute it and/or modify
+*       it under the terms of the LGPL license as published by
+*       the Free Software Foundation, either version 3
+*       of the License, or (at your option) any later version.
+*       This software is distributed in the hope that it will be useful,
+*       but WITHOUT ANY WARRANTY without even the implied warranty of
+*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*       LGPL License for more details.
 *
-*	You should have received a copy of the GNU Lesser General Public License version 3 and the
-*	General Public License version 3 along with this program.
-*	If not, see <http://www.gnu.org/licenses/>.
+*       You should have received a copy of the GNU Lesser General Public License version 3 and the
+*       General Public License version 3 along with this program.
+*       If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 // rpoly_ak1.cpp - Program for calculating the roots of a polynomial of real coefficients.
 // Written in Visual C++ 2005 Express Edition
