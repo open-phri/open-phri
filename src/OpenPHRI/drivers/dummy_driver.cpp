206334af2ef906294faf01fc93ad2e7909319533
Benjamin Navarro <navarro.benjamin13@gmail.com>
2020-04-02 16:21:08 +0200

    feat: DummyDriver now use a default pose if not provided

--- src/OpenPHRI/drivers/dummy_driver.cpp
+++ src/OpenPHRI/drivers/dummy_driver.cpp
@@ -4,9 +4,9 @@
 #include <yaml-cpp/yaml.h>
 #include <thread>
 #include <chrono>
+#include <iostream>
 
-using namespace phri;
-using namespace std;
+namespace phri {
 
 bool DummyDriver::registered_in_factory =
     phri::DriverFactory::add<DummyDriver>("dummy");
@@ -21,16 +21,18 @@ DummyDriver::DummyDriver(phri::Robot& robot, const YAML::Node& configuration)
     const auto& driver = configuration["driver"];
 
     if (driver) {
-        try {
-            setTimeStep(driver["sample_time"].as<double>());
-        } catch (...) {
+        const auto& sample_time_node = driver["sample_time"];
+        if (sample_time_node) {
+            setTimeStep(sample_time_node.as<double>());
+        } else {
             throw std::runtime_error(
                 OPEN_PHRI_ERROR("You must provide a 'sample_time' field in the "
                                 "driver configuration."));
         }
-        try {
+        auto init_joint_positions_node = driver["init_joint_positions"];
+        if (init_joint_positions_node) {
             auto init_joint_positions =
-                driver["init_joint_positions"].as<std::vector<double>>();
+                init_joint_positions_node.as<std::vector<double>>();
             if (init_joint_positions.size() != robot_.jointCount()) {
                 throw std::runtime_error(OPEN_PHRI_ERROR(
                     "The number of values in 'init_joint_positions' does not "
@@ -40,10 +42,12 @@ DummyDriver::DummyDriver(phri::Robot& robot, const YAML::Node& configuration)
                 jointState().position()(i) =
                     init_joint_positions[i] * M_PI / 180.;
             }
-        } catch (...) {
-            throw std::runtime_error(
-                OPEN_PHRI_ERROR("You must provide a 'init_joint_positions' "
-                                "field in the driver configuration."));
+        } else {
+            jointState().position().setConstant(1.);
+            std::cout << "\n"
+                      << OPEN_PHRI_WARNING(
+                             "'init_joint_positions' was not given, using ")
+                      << jointState().position().transpose() << std::endl;
         }
     } else {
         throw std::runtime_error(OPEN_PHRI_ERROR(
@@ -77,3 +81,5 @@ bool DummyDriver::send() {
     jointState().position() += jointCommand().velocity() * getTimeStep();
     return true;
 }
+
+} // namespace phri
